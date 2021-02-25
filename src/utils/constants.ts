51e99f4e25f5985cf1fdbe46da0dedfaebd5b1f6
Germán Martínez <germaga92@gmail.com>
2021-02-25 18:56:18 +0100

    Update Refunder contract

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers'
 
-export const REFUNDER_ADDRESS = '0x2d8cE02dd1644A9238e08430CaeA15a609503140'
+export const REFUNDER_ADDRESS = '0x0971BF033F429B6077b81911505c406Ffb8cde2c'
 export const WETH_ADDRESS = '0xc778417E063141139Fce010982780140Aa0cD5Ab'
 export const MIN_FUNDS = ethers.utils.parseEther('0.001')
 
